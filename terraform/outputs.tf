a5f9c1a567da8230eb177bfbbd21597e065d5782
Nafreen Anjum <nafreen.anjum@wisseninfotech.com>
2026-06-01 16:28:23 +0530

    Implement modular Terraform architecture

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,19 @@
 output "project_name" {
   value = "EC2 Monitor Project"
 }
+
+output "server1_id" {
+  value = module.ec2.server1_id
+}
+
+output "server2_id" {
+  value = module.ec2.server2_id
+}
+
+output "server3_id" {
+  value = module.ec2.server3_id
+}
+
+output "lambda_name" {
+  value = module.lambda.lambda_name
+}
