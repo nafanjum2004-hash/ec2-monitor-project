8beebc8f98f6379f7491524c0449ea4e53007bfd
Nafreen Anjum <nafreen.anjum@wisseninfotech.com>
2026-06-01 12:35:00 +0530

    Refactor project structure

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -6,3 +6,12 @@ resource "aws_instance" "server1" {
     Name = "Server-1"
   }
 }
+
+resource "aws_instance" "server2" {
+  ami           = "ami-0f58b397bc5c1f2e8"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "Server-2"
+  }
+}
