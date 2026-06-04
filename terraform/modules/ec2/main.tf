bc2df0035540b4781292ab296f80dfb06fd55479
Nafreen Anjum <nafreen.anjum@wisseninfotech.com>
2026-06-04 15:58:21 +0530

    Added default security group

--- terraform/modules/ec2/main.tf
+++ terraform/modules/ec2/main.tf
@@ -1,9 +1,14 @@
+data "aws_security_group" "default" {
+  name = "default"
+}
+
 resource "aws_instance" "server" {
 count = var.instance_count
 
 ami           = var.ami_id
 instance_type = var.instance_type
 key_name = var.key_name
+vpc_security_group_ids = [data.aws_security_group.default.id]
 
 tags = {
 Name = "Server-${count.index + 1}"
