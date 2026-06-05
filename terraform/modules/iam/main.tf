2efec3098fc02e20f8d557408554cddeba635c8f
Nafreen Anjum <nafreen.anjum@wisseninfotech.com>
2026-06-05 11:48:41 +0530

    Implemented GitHub OIDC authentication

--- terraform/modules/iam/main.tf
+++ terraform/modules/iam/main.tf
@@ -2,16 +2,7 @@ resource "aws_iam_role" "lambda_role" {
   name = var.role_name
 
 
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Action = "sts:AssumeRole"
-      Effect = "Allow"
-      Principal = {
-        Service = "lambda.amazonaws.com"
-      }
-    }]
-  })
+assume_role_policy = file("${path.module}/policy.json")
 }
 
 resource "aws_iam_role_policy_attachment" "lambda_basic" {
